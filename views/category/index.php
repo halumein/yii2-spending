baa075536c28cc6fa23d658e42bae780a63c520d
Trishkin Vadim <halumein@gmail.com>
2016-10-18 11:36:09 +0500

    add menu

--- views/category/index.php
+++ views/category/index.php
@@ -1,6 +1,7 @@
 <?php
 
 use yii\helpers\Html;
+use yii\helpers\Url;
 use yii\grid\GridView;
 use halumein\spending\models\Category;
 
@@ -15,30 +16,45 @@ $this->params['breadcrumbs'][] = $this->title;
 
     <?php // echo $this->render('_search', ['model' => $searchModel]); ?>
 
-    <p>
-        <?php echo Html::a('Добавить категорию', ['create'], ['class' => 'btn btn-success']) ?>
-    </p>
+    <div class="row">
+        <div class="col-sm-4 col-md-3">
+            <p>
+                <?php echo Html::a('Добавить категорию', ['create'], ['class' => 'btn btn-success']) ?>
+            </p>
+        </div>
+        <div class="col-sm-8 col-md-9" >
+            <ul class="nav nav-pills pull-right">
+                <li class="active"><a href="<?= Url::to(['/spending/category/index']) ?>">Категории</a></li>
+                <li><a href="<?= Url::to(['/spending/spending/index']) ?>">Затраты</a></li>
+            </ul>
+        </div>
+    </div>
 
-    <?php echo GridView::widget([
-        'dataProvider' => $dataProvider,
-        'filterModel' => $searchModel,
-        'columns' => [
+    <div class="row">
+        <div class="col-sm-12">
+            <?php echo GridView::widget([
+                'dataProvider' => $dataProvider,
+                'filterModel' => $searchModel,
+                'columns' => [
 
-            ['attribute' => 'id', 'filter' => false, 'options' => ['style' => 'width: 55px;']],
-            'name',
-            [
-                'attribute' => 'parent_id',
-                'filter' => Html::activeDropDownList(
-                    $searchModel,
-                    'parent_id',
-                    Category::buildTextTree(),
-                    ['class' => 'form-control', 'prompt' => 'Все категории']
-                ),
-                'value' => 'parent.name'
-            ],
+                    ['attribute' => 'id', 'filter' => false, 'options' => ['style' => 'width: 55px;']],
+                    'name',
+                    [
+                        'attribute' => 'parent_id',
+                        'filter' => Html::activeDropDownList(
+                            $searchModel,
+                            'parent_id',
+                            Category::buildTextTree(),
+                            ['class' => 'form-control', 'prompt' => 'Все категории']
+                        ),
+                        'value' => 'parent.name'
+                    ],
 
-            ['class' => 'yii\grid\ActionColumn', 'template' => '{update} {delete}',  'buttonOptions' => ['class' => 'btn btn-default'], 'options' => ['style' => 'width: 105px;']],
-        ],
-    ]); ?>
+                    ['class' => 'yii\grid\ActionColumn', 'template' => '{update} {delete}',  'buttonOptions' => ['class' => 'btn btn-default'], 'options' => ['style' => 'width: 105px;']],
+                ],
+            ]); ?>
+        </div>
+    </div>
+
 
 </div>
--- views/spending/index.php
+++ views/spending/index.php
@@ -1,6 +1,7 @@
 <?php
 
 use yii\helpers\Html;
+use yii\helpers\Url;
 use yii\grid\GridView;
 use halumein\spending\models\Category;
 use yii\helpers\ArrayHelper;
@@ -16,53 +17,69 @@ $this->params['breadcrumbs'][] = $this->title;
 
     <?php // echo $this->render('_search', ['model' => $searchModel]); ?>
 
-    <p>
-        <?php echo Html::a('Добавить затрату', ['create'], ['class' => 'btn btn-success']) ?>
-    </p>
 
-    <?php echo GridView::widget([
-        'dataProvider' => $dataProvider,
-        'filterModel' => $searchModel,
-        'columns' => [
+    <div class="row">
+        <div class="col-sm-4 col-md-3">
+        <p>
+            <?php echo Html::a('Добавить затрату', ['create'], ['class' => 'btn btn-success']) ?>
+        </p>
+        </div>
+        <div class="col-sm-8 col-md-9" >
+            <ul class="nav nav-pills pull-right">
+                <li><a href="<?= Url::to(['/spending/category/index']) ?>">Категории</a></li>
+                <li class="active"><a href="<?= Url::to(['/spending/spending/index']) ?>">Затраты</a></li>
+            </ul>
+        </div>
+    </div>
 
-            ['attribute' => 'id', 'filter' => false, 'options' => ['style' => 'width: 55px;']],
-            'name',
-            [
-                'attribute' => 'category_id',
-                'filter' => Html::activeDropDownList(
-                    $searchModel,
-                    'category_id',
-                    Category::buildTextTree(),
-                    ['class' => 'form-control', 'prompt' => 'Все категории']
-                ),
-                'value' => 'category.name'
-            ],
-            ['attribute' => 'date', 'filter' => false],
-            ['attribute' => 'amount', 'filter' => false, 'options' => ['style' => 'width: 70px;']],
-            ['attribute' => 'cost', 'filter' => false, 'options' => ['style' => 'width: 100px;']],
-            [
-                'attribute' => 'cashbox_id',
-                'filter' => Html::activeDropDownList(
-                    $searchModel,
-                    'cashbox_id',
-                    ArrayHelper::map($activeCashboxes, 'id', 'name'),
-                    ['class' => 'form-control', 'prompt' => 'Все кассы']
-                ),
-                'value' => 'cashbox.name'
-            ],
-            [
-                'attribute' => 'user_id',
-                'filter' => Html::activeDropDownList(
-                    $searchModel,
-                    'user_id',
-                    ArrayHelper::map($activeUsers, 'id', 'name'),
-                    ['class' => 'form-control', 'prompt' => 'Все сотрудники']
-                ),
-                'value' => 'user.fullName'
-            ],
+    <div class="row">
+        <div class="col-sm-12">
+            <?php echo GridView::widget([
+                'dataProvider' => $dataProvider,
+                'filterModel' => $searchModel,
+                'columns' => [
 
-            //['class' => 'yii\grid\ActionColumn'],
-        ],
-    ]); ?>
+                    ['attribute' => 'id', 'filter' => false, 'options' => ['style' => 'width: 55px;']],
+                    'name',
+                    [
+                        'attribute' => 'category_id',
+                        'filter' => Html::activeDropDownList(
+                            $searchModel,
+                            'category_id',
+                            Category::buildTextTree(),
+                            ['class' => 'form-control', 'prompt' => 'Все категории']
+                        ),
+                        'value' => 'category.name'
+                    ],
+                    ['attribute' => 'date', 'filter' => false],
+                    ['attribute' => 'amount', 'filter' => false, 'options' => ['style' => 'width: 70px;']],
+                    ['attribute' => 'cost', 'filter' => false, 'options' => ['style' => 'width: 100px;']],
+                    [
+                        'attribute' => 'cashbox_id',
+                        'filter' => Html::activeDropDownList(
+                            $searchModel,
+                            'cashbox_id',
+                            ArrayHelper::map($activeCashboxes, 'id', 'name'),
+                            ['class' => 'form-control', 'prompt' => 'Все кассы']
+                        ),
+                        'value' => 'cashbox.name'
+                    ],
+                    [
+                        'attribute' => 'user_id',
+                        'filter' => Html::activeDropDownList(
+                            $searchModel,
+                            'user_id',
+                            ArrayHelper::map($activeUsers, 'id', 'name'),
+                            ['class' => 'form-control', 'prompt' => 'Все сотрудники']
+                        ),
+                        'value' => 'user.fullName'
+                    ],
+
+                    //['class' => 'yii\grid\ActionColumn'],
+                ],
+            ]); ?>
+        </div>
+    </div>
+
 
 </div>
